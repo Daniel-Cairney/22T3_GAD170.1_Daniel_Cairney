65e7341c0b65f3fd035d4248f2e208752e2ad5cc
Daniel-Cairney <114543733+Daniel-Cairney@users.noreply.github.com>
2022-10-05 14:30:30 +1100

    added stuff

--- Assets/Scripts by Daniel/DanielsLevellingSystem.cs
+++ Assets/Scripts by Daniel/DanielsLevellingSystem.cs
@@ -7,7 +7,7 @@ namespace DanielCairney
     /// <summary>
     /// This class holds all the logic for our levelling system, so that includes logic to handle levelling up, gaining XP and detecting when we should level up.
     /// </summary>
-    public class LevellingSystem : MonoBehaviour
+    public class DanielsLevellingSystem : MonoBehaviour
     {
         // TODO XP 1/13 Declare a new variable to track the current amount of XP we have accumulated (our current XP).
         private float currentExperience;
@@ -29,15 +29,17 @@ namespace DanielCairney
             Debug.Log("Current Experience " + currentExperience);
             Debug.Log("Level Up Threshold" + levelUpThreshold);
             // TODO XP 8/13: Increase the current XP by a random amount between 50 and 100.
-
+            currentExperience += Random.Range(50 , 100);
             // TODO XP 9/13: Debug out our current XP.
-
+            Debug.Log("Current XP " + currentExperience);
             // TODO XP 10/13: Check if our current XP is more than our threshold.
-
+            if (currentExperience > levelUpThreshold) {
+                currentLevel += 1; 
+            }
             // TODO XP 11/13: If it is, then let's increase out level by one.
-
+        
             // TODO XP 12/13: Let's also increase recalculate our current XP threshold as we've levelled up.
-
+           
             // TODO XP 13/13: Debug out our new level value, as well as our current XP and the next threshold we need to hit.
 
             // TODO XP Final: Add code comments describing what you hope your code is doing throughout this script.
